b0be956381b9785309c84c19f9136d810fc5ab66
Mayara-Arantes <mayaraarantes9@gmail.com>
2024-01-09 22:47:59 -0300

    desafio finalizado

--- src/index.js
+++ src/index.js
@@ -1,31 +1,32 @@
-let nome = "Mayara"
-let nivelXp = 5001
+let nome = "Mayara";
+let nivelXp = 10002;
+
+console.log("O Herói de nome " + nome + " está no nível de ");
 
 if (nivelXp < 1000) {
-  console.log("Ferro")
-  
-}else if (nivelXp === 1000 && 2000) {
-  console.log("Bronze")
-  
-}else if (nivelXp === 2001 && 5000){
-  console.log("Prata")
-  
-}else if (nivelXp === 5001 && 7000) {
-  console.log("Ouro")
-  
-}else if (nivelXp === 7001 && 8000) {
-  console.log("Platina")
- 
-}else if (nivelXp === 8001 && 9000) {
-  console.log("Ascendente")
-  
-}else if (nivelXp === 9001 && 10000) {
-  console.log("Imortal")
-  
-}else if (nivelXp >= 10001) {
-  console.log("Radiante")
-  
-}else 
-    console.log("Nivel não encontrado")
- 
-console.log("O Heroi de nome " + nome + " está no nivel de " + nivelXp)
+    console.log("Ferro");
+
+} else if (nivelXp >= 1001 && nivelXp <= 2000) {
+    console.log("Bronze");
+    
+} else if (nivelXp > 2001 && nivelXp <= 5000) {
+    console.log("Prata");
+    
+} else if (nivelXp > 5001 && nivelXp <= 7000) {
+    console.log("Ouro");
+    
+} else if (nivelXp > 7001 && nivelXp <= 8000) {
+    console.log("Platina");
+    
+} else if (nivelXp > 8001 && nivelXp <= 9000) {
+    console.log("Ascendente");
+    
+} else if (nivelXp > 9001 && nivelXp <= 10000) {
+    console.log("Imortal");
+    
+} else if (nivelXp > 10001) {
+    console.log("Radiante");
+    
+} else {
+    console.log("Nível não encontrado");
+}
